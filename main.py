55b5baa9aca2840cafeb9200944a03ac1fcf3ac8
Johnathon Brumfield <91100117+JohnathonB6573@users.noreply.github.com>
2023-05-05 12:25:11 -0500

    Classes

---
+++ main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import random
+
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_directions(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
